4fac515d090540233e29cb054bf324e5f5a50cd9
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-08-03 16:39:59 -0300

    fix: title

--- pruebas/01-reading-list/joser098/src/components/Header.tsx
+++ pruebas/01-reading-list/joser098/src/components/Header.tsx
@@ -3,7 +3,7 @@ import Filters from "./Filters";
 const Header = () => {
   return (
     <header className="bg-black">
-      <h1 className="text-3xl text-white font-bold p-3">BOOKS WOLRD</h1>
+      <h1 className="text-3xl text-white font-bold p-3">BOOKS WORLD</h1>
       <Filters />
     </header>
   );
