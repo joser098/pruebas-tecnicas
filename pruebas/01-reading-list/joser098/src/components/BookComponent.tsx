882695dbbd6d8fd5a1cd6a8e787a886cabafdfb6
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-08-04 16:57:39 -0300

    hover added

--- pruebas/01-reading-list/joser098/src/components/BookComponent.tsx
+++ pruebas/01-reading-list/joser098/src/components/BookComponent.tsx
@@ -5,8 +5,8 @@ const BookComponent: React.FC<Book> = ({ title, cover }) => {
 
   return (
     <main className="inline-block p-3 " >
-      <div className="">
-        <img className="object-cover w-full h-64" src={cover} alt={title} />
+      <div className="relative flex justify-center">
+        <img className="object-cover w-full h-64 hover:opacity-70 transition-opacity" src={cover} alt={title} />
       </div>
     </main>
   );
